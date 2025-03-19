dfcab31c932f4cae52ff53f0dac23aa571e642a6
AN SYU LI <yessir0621@gmail.com>
2025-03-19 18:52:18 +0800

    docs: version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='geo_tools',
-    version='0.1.0',
+    version='0.1.1',
     description='A geospatial tool for looking up countries from coordinates.',
     author='An-Syu Li',
     author_email='yessir0621@gmail.com',
-    packages=find_packages(),  # Automatically discovers packages in your project
+    packages=find_packages(include=['geo_tools', 'geo_tools.*']),  # Automatically discovers packages in your project
     install_requires=[
         'pandas==1.5.3',
         'numpy==1.26.4',
